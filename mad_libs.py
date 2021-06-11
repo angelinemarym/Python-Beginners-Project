8f0332f1bbc91056dbac17baf557326f96e357cf
Angeline Mary Marchella <82133134+angelinemarym@users.noreply.github.com>
2021-06-11 14:24:00 +0700

    Add files via upload

---
+++ mad_libs.py
@@ -0,0 +1,9 @@
+nama_hewan = input("Sebutkan salah satu nama hewan: ")
+transportasi = input("Sebutkan salah satu jenis transportasi: ")
+nama_artis = input("Sebutkan salah satu nama artis: ")
+nama_orang = input("Sebutkan salah satu nama orang yang kamu kenal: ")
+
+print("Ada kera mirip " + nama_hewan)
+print("Keduanya naik " + transportasi)
+print("Dikira mirip " + nama_artis)
+print("Ternyata yang dilirik " + nama_orang)
